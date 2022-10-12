a206a258e7a85c2fb17f323acf0f185e4f1bf57d
asadtariq-dev <asad.tariq@devsinc.com>
2022-10-12 23:50:55 +0500

    Added Register Page

---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import AvatarLogo from "../../img/addAvatar.png";
+
+const Register = () => {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <p className="logo">Infinity Chat</p>
+        <form>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={AvatarLogo} width="40" alt="avatar" />
+            <span>Add avatar</span>
+          </label>
+          <button>Sign up</button>
+        </form>
+        <p>Already have an account? Login</p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
