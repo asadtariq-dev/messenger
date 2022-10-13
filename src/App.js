e8c1a3b4adae3498a90cdfdb9cca5c87737c0198
asadtariq-dev <asad.tariq@devsinc.com>
2022-10-13 13:16:58 +0500

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles.scss";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-function App() {
-  return <div className="App">chat</div>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
