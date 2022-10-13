b2a547456307f8ac726c4b6902dbb33b5e5aee54
asadtariq-dev <asad.tariq@devsinc.com>
2022-10-13 12:57:06 +0500

    Modified Register page
    * Changed Logo text
    * Styled avatar input

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,16 +5,18 @@ const Register = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <p className="logo">Infinity Chat</p>
+        <p className="logo">Puprple</p>
         <form>
           <input type="text" placeholder="Name" />
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
           <input style={{ display: "none" }} type="file" id="file" />
-          <label htmlFor="file">
-            <img src={AvatarLogo} width="40" alt="avatar" />
-            <span>Add avatar</span>
-          </label>
+          <div style={{ border: '1px solid whitesmoke', padding: '5px', borderRadius: '10px', color: 'purple'}}>
+            <label htmlFor="file">
+              <img src={AvatarLogo} width="40" alt="avatar" />
+              <span>Add an avatar</span>
+            </label>
+          </div>
           <button>Sign up</button>
         </form>
         <p>Already have an account? Login</p>
