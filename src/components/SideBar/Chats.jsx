d066b52664a248838339552558f3a4cf14081a57
asadtariq-dev <asad.tariq@devsinc.com>
2022-10-13 15:40:11 +0500

    Added a Chats component

---
+++ src/components/SideBar/Chats.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Chats = () => {
+  return (
+    <div className="chats">
+      <div className="users">
+        <img
+          src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          alt=""
+        />
+        <div className="userInfo">
+          <span>James</span>
+          <p>Hello there</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chats;
