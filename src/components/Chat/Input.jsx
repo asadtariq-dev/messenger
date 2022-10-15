bff3b59036d6cc6b0b3a5ddda242c26b71e04770
asadtariq-dev <asad.tariq@devsinc.com>
2022-10-15 09:45:56 +0500

    Added Firebase db and storage
    * Removed dummy data
    * Added firebase database

--- src/components/Chat/Input.jsx
+++ src/components/Chat/Input.jsx
@@ -1,20 +1,105 @@
-import React from "react";
+import {
+  arrayUnion,
+  doc,
+  serverTimestamp,
+  Timestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { ChatContext } from "../../context/ChatContext";
+import { db, storage } from "../../firebase";
 import attachmentIcon from "../../img/attachment.png";
 import imgIcon from "../../img/img.png";
 import sendIcon from "../../img/send.png";
+import { v4 as uuid } from "uuid";
 
 const Input = () => {
+  const [text, setText] = useState("");
+  const [image, setImage] = useState("");
+
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const handleSend = async () => {
+    console.log(data);
+    if (image) {
+      const storageRef = ref(storage, uuid());
+      const uploadTask = uploadBytesResumable(storageRef, image);
+
+      uploadTask.on(
+        (error) => {
+          console.log(error.message);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (url) => {
+            await updateDoc(doc(db, "chats", data.chatId), {
+              messages: arrayUnion({
+                id: uuid(),
+                text,
+                senderId: currentUser.uid,
+                date: Timestamp.now(),
+                image: url,
+              }),
+            });
+          });
+        }
+      );
+    } else {
+      await updateDoc(doc(db, "chats", data.chatId), {
+        messages: arrayUnion({
+          id: uuid(),
+          text,
+          senderId: currentUser.uid,
+          date: Timestamp.now(),
+        }),
+      });
+    }
+
+    await updateDoc(doc(db, "userChats", currentUser.uid), {
+      [data.chatId + ".lastMessage"]: {
+        text,
+      },
+      [data.chatId + ".date"]: serverTimestamp(),
+    });
+    await updateDoc(doc(db, "userChats", data.user.uid), {
+      [data.chatId + ".lastMessage"]: {
+        text,
+      },
+      [data.chatId + ".date"]: serverTimestamp(),
+    });
+    setText("");
+    setImage(null);
+  };
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSend();
+  };
   return (
     <div className="input">
-      <input type="text" placeholder="message..." />
+      <input
+        type="text"
+        placeholder="message..."
+        onChange={(event) => setText(event.target.value)}
+        value={text}
+        onKeyDown={handleKey}
+      />
       <div className="send">
         <img src={imgIcon} alt="" />
-        <input type="file" style={{ display: "none" }} id="file" />
+        <input
+          type="file"
+          style={{ display: "none" }}
+          id="file"
+          onChange={(event) => setImage(event.target.files[0])}
+          value={image}
+        />
         <label htmlFor="file">
           <img src={attachmentIcon} alt="" />
         </label>
-        <button className="text">Send</button>
-        <img className="icon" src={sendIcon} alt="" />
+        <button className="text" onClick={handleSend}>
+          Send
+        </button>
+        <img className="icon" src={sendIcon} alt="" onClick={handleSend} />
       </div>
     </div>
   );
--- src/components/SideBar/SearchBar.jsx
+++ src/components/SideBar/SearchBar.jsx
@@ -1,22 +1,97 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import searchIcon from "../../img/search.png";
+import {
+  collection,
+  setDoc,
+  getDocs,
+  doc,
+  getDoc,
+  query,
+  where,
+  updateDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+import { db } from "../../firebase";
+import { AuthContext } from "./../../context/AuthContext";
 
 const SearchBar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(false);
+
+  const { currentUser } = useContext(AuthContext);
+
+  const handleSearch = async (str) => {
+    setSearchQuery(str);
+    const q = query(collection(db, "users"), where("displayName", "==", str));
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (error) {
+      setError(true);
+    }
+  };
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+
+  const handleSelect = async () => {
+    const chatID =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+    try {
+      const res = await getDoc(doc(db, "chats", chatID));
+      if (!res.exists()) {
+        await setDoc(doc(db, "chats", chatID), { messages: [] });
+
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [chatID + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [chatID + ".date"]: serverTimestamp(),
+        });
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [chatID + ".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [chatID + ".date"]: serverTimestamp(),
+        });
+      }
+    } catch (error) {
+      setError(true);
+    }
+    setUser("");
+    setSearchQuery("");
+  };
+
   return (
     <>
-      <input type="text" placeholder="search" />
-      {/* <img src={searchIcon} style={{ width: "30px", height: "30px" }} alt="" /> */}
-      {/* <div className="searchBar">
-        <div className="users">
-          <img
-            src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-            alt=""
-          />
-          <div className="userInfo">
-            <span>James</span>
+      <input
+        type="text"
+        placeholder="search"
+        onKeyDown={handleKey}
+        onChange={(e) => handleSearch(e.target.value)}
+        value={searchQuery}
+      />
+      {/* <img src={searchIcon} style={{ width: "30px", height: "30px" }} alt="" />  */}
+      {/* {error && <p>User not found</p>} */}
+      {user && (
+        <div className="searchBar">
+          <div className="users" onClick={handleSelect}>
+            <img src={user.photoURL} alt="" />
+            <div className="userInfo">
+              <span>{user.displayName}</span>
+            </div>
           </div>
         </div>
-      </div> */}
+      )}
     </>
   );
 };
