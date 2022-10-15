31f371fcd5b649a35de3aaf31049edd99d3ba795
asadtariq-dev <asad.tariq@devsinc.com>
2022-10-15 09:42:53 +0500

    Added contextAPI and protected routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import "./styles.scss";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 const App = () => {
+  const { currentUser } = useContext(AuthContext);
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to={"/login"} />;
+    }
+    return children;
+  };
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route
+          exact
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/register" element={<Register />} />
       </Routes>
