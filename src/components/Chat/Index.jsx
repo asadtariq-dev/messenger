5388ec9dd7b66c032b7835c753bb74dc04d993dd
asadtariq-dev <asad.tariq@devsinc.com>
2022-10-18 21:57:00 +0500

    Added Suggestions for new users

--- src/components/Chat/Index.jsx
+++ src/components/Chat/Index.jsx
@@ -3,18 +3,11 @@ import Messages from "./Messages";
 import ChatHead from "./ChatHead";
 import Input from "./Input";
 import { ChatContext } from "../../context/ChatContext";
+import Suggestion from "./Suggestion";
 const Chat = () => {
   const { data } = useContext(ChatContext);
 
-  if (data.chatId === "null")
-    return (
-      <div
-        className="chat"
-        style={{ textAlign: "center", alignSelf: "center" }}
-      >
-        <p>Select a user to start chatting</p>
-      </div>
-    );
+  if (data?.chatId === "null") return <Suggestion />;
   return (
     <div className="chat" style={{ textAlign: "center" }}>
       <>
---
+++ src/components/Chat/Suggestion.jsx
@@ -0,0 +1,63 @@
+import { collection, onSnapshot } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { ChatContext } from "../../context/ChatContext";
+import { db } from "../../firebase";
+import Loader from "../Loader";
+
+const Suggestion = () => {
+  const [users, setUsers] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+
+  const { dispatch } = useContext(ChatContext);
+
+  const handleSelect = (user) => {
+    dispatch({ type: "CHANGE_USER", payload: user });
+  };
+  useEffect(() => {
+    const users = [];
+    const unsub = onSnapshot(collection(db, "users"), (collection) => {
+      collection.forEach((doc) => {
+        if (doc.data().uid !== currentUser.uid) {
+          users.push(doc.data());
+        }
+      });
+      setUsers(users);
+    });
+
+    return () => {
+      unsub();
+    };
+  }, []);
+  return (
+    <div className="chat" style={{ textAlign: "center", alignSelf: "center" }}>
+      <p>People you may know</p>
+      <div className="suggestions">
+        {users?.map((user) => {
+          return (
+            <div
+              className="user"
+              key={user.uid}
+              onClick={() => handleSelect(user)}
+            >
+              {user?.photoURL ? (
+                <img src={user.photoURL} alt="" width={20} />
+              ) : (
+                <Loader shape="circle" />
+              )}
+              <div className="userInfo">
+                {user.displayName ? (
+                  <span className="name">{user.displayName}</span>
+                ) : (
+                  <Loader shape="rect" />
+                )}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Suggestion;
