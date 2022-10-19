8d833b8eed4bcdd730bba33db44678613bb8221b
asadtariq-dev <asad.tariq@devsinc.com>
2022-10-19 06:01:15 +0500

    Added database URL

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
@@ -6,11 +7,12 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyChMl5_8awPbKVb5Rh_KlYn6we2t9we5D0",
   authDomain: "purple-2-f2597.firebaseapp.com",
+  databaseURL: "https://purple-2-f2597-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "purple-2-f2597",
   storageBucket: "purple-2-f2597.appspot.com",
   messagingSenderId: "433287893540",
   appId: "1:433287893540:web:9c3bb71925bc6844a03eb1",
-  measurementId: "G-EL9PYE93FX",
+  measurementId: "G-EL9PYE93FX"
 };
 
 export const app = initializeApp(firebaseConfig);
